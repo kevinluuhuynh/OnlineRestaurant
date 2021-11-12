161e5d03aa334a8b612536281924d554464a16ad
wtswain1 <91905227+wtswain1@users.noreply.github.com>
2021-11-12 17:17:50 -0500

    cleaned up by removing old files

---
+++ database_files/individual_tables/categories.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.9
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 09, 2021 at 02:33 AM
+-- Server version: 5.6.37
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `category` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category`) VALUES
+('APPETIZERS'),
+('BEEF'),
+('CHICKEN'),
+('CHOW MEIN'),
+('DIET MENU SPECIAL'),
+('EGG FOO YOUNG'),
+('FRIED RICE'),
+('HO FUN OR MEI FUN'),
+('KY LIN CHEF SUGGESTIONS'),
+('LO MEIN'),
+('NOODLE SOUPS'),
+('PAN FRIED EGG NOODLE'),
+('PORK'),
+('SEAFOOD'),
+('SIDE ORDERS & DRINKS'),
+('SOUPS'),
+('SPECIAL PLATTER'),
+('SWEET & SOUR'),
+('VEGETABLES & BEAN CURD');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
