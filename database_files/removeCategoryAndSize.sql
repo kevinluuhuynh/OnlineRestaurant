23c46780518533557a78102ca4211bed44260e42
lopezc13 <80978156+lopezc13@users.noreply.github.com>
2021-12-02 23:13:51 -0500

    Add files via upload
    Drop's category and size 
    might cause error without updating other tables will check in lab

---
+++ database_files/removeCategoryAndSize.sql
@@ -0,0 +1,14 @@
+
+
+
+
+
+-- this might effect foreign keys do not use yet
+-- would need to update menu and set values to null?
+-- I will check with you guys tomorrow
+DROP TABLE 'categories';
+
+DROP TABLE 'size';
+
+
+
